261e83836710d0b5979842df99414c6ff4d4185c
Adam Dean <adam.dean@chem.ox.ac.uk>
2020-07-27 12:26:33 +0100

    Added in function search

--- cassearch.sh
+++ cassearch.sh
@@ -5,26 +5,30 @@ for arg in "$@" ; do
   case "$arg" in
     "--help"      ) set -- "$@" "-h"   ;;
     "--subroutine") set -- "$@" "-s"   ;;
-    "--call"      ) set -- "$@" "-c"   ;;
+    "--function"  ) set -- "$@" "-f"   ;;
     "--no-locate" ) set -- "$@" "-L"   ;;
     *             ) set -- "$@" "$arg" ;;
   esac
 done
 
 print_help=false ;
-subroutine_search=false ;
-call_search=false ;
+subroutine_search=true ;
+function_search=true ;
 no_locate=false ;
-while getopts ":hscL" opt ; do
+while getopts ":hgsfL" opt ; do
   case ${opt} in
     h ) # Print help and exit.
       print_help=true ;
     ;;
-    s ) # Subroutine search.
-      subroutine_search=true ;
+    g ) # Just grep.
+      subroutine_search=false ;
+      function_search=false ;
     ;;
-    c ) # Call search.
-      call_search=true ;
+    s ) # Subroutine only search.
+      function_search=false ;
+    ;;
+    f ) # Function only search.
+      subroutine_search=false ;
     ;;
     L ) # Do not locate subroutine.
       no_locate=true ;
@@ -40,24 +44,25 @@ done
 
 [ $OPTIND != 1 ] && shift $((OPTIND-1)) ;
 
+
+
 if $print_help ; then
     echo 'Usage: cassearch OPTION... [KEYWORD]...' ;
     echo ;
     echo 'Search for keywords in the files in the current directory.' ;
     echo 'Options optimised best for use in the CASTEP source code.' ;
     echo ;
-    echo '-c, --call           searches files for calls of the keyword' ;
+    echo '-f, --function       searches and locates for functions only'
+    echo '-g, --grep           just greps the files' ;
     echo '-h, --help           prints this help and exits' ;
     echo '-L, --no-locate      will not vim into subroutine if found, redundant without --subroutine' ;
-    echo '-s, --subroutine     searches files for subroutine and locates using vim' ;
+    echo '-s, --subroutine     searches and locates for subroutines only' ;
     exit 0 ;
+fi
 
-elif $call_search ; then
-    echo "'call' feature has not been implemented yet." ;
-    echo 'Exiting.'
-    exit 1 ;
 
-elif $subroutine_search ; then
+
+if $subroutine_search ; then
     if $no_locate ; then
         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done ;
     else
@@ -65,8 +70,8 @@ elif $subroutine_search ; then
 
         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "subroutine\s+${1}\s*\(" "$i" ; done >> $temp_file ;
 
-        num_lines=`cat $temp_file | wc -l` ;
-        if [[ $num_lines == 1 ]] ; then
+        num_lines_sub=`cat $temp_file | wc -l` ;
+        if [[ $num_lines_sub == 1 ]] ; then
             file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
             line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
             vim +$line_num $file_to_open ;
@@ -76,9 +81,38 @@ elif $subroutine_search ; then
             for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done ;
         fi
     fi
+fi
 
-else
+
+
+if $function_search ; then
+    if $no_locate ; then
+         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done ;
+     else
+         temp_file=`mktemp` ;
+
+         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "function\s+${1}\s*\(" "$i" ; done >> $temp_file ;
+
+         num_lines_func=`cat $temp_file | wc -l` ;
+         if [[ $num_lines_func == 1 ]] ; then
+             file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
+             line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
+             vim +$line_num $file_to_open ;
+
+             exit 0 ;
+         else
+             for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done ;
+         fi
+     fi
+fi
+
+
+
+if ( ! $subroutine_search && ! $function_search ) || [[ $((num_lines_sub+num_lines_func)) == 0 ]] ; then
     for i in * ; do [ -f "$i" ] && \grep --line-number --with-filename --color=auto "${1}" "${i}" ; done
 fi
 
+
+
 exit 0 ;
+
