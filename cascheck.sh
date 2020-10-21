b42964acb19bb5600b5363021220970938c84352
Adam Dean <adam.dean@chem.ox.ac.uk>
2020-10-22 00:58:58 +0100

    Improved cascheck so now prints directory of process(es)

--- cascheck.sh
+++ cascheck.sh
@@ -1,3 +1,15 @@
-output=`ps -aux | \grep "castep" | \grep --invert-match "grep"` ;
 
-[ "$output" ] && echo 'USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND' && echo "$output" | \grep --color=auto --extended-regexp "castep(\.mpi|\.serial)? +\w+$" ;
+IDs=( `\pgrep castep` ) ;
+
+#[ ${#IDs[@]} -gt 0 ] && echo 'some header i could add'
+
+for ID in "${IDs[@]}" ; do
+  direc=`pwdx "$ID"` ;
+  cmmnd=`ps "$ID" | \grep --only-matching --color=always --extended-regexp "castep(\.mpi|\.serial)? +\w+$"` ;
+  echo "$direc $cmmnd" ;
+done
+
+
+#output=`ps -auxwwwf | \grep "castep" | \grep --invert-match "grep"` ;
+
+#[ "$output" ] && echo 'USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND' && echo "$output" | \grep --color=auto --extended-regexp "castep(\.mpi|\.serial)? +\w+$" ;
