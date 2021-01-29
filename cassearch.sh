d524d04ee650ef8662ea7c8dba6b4c9d79d207f1
Adam Dean <adam.dean@chem.ox.ac.uk>
2021-01-29 15:38:52 +0000

    Added interface functionality to cassearch

--- cassearch.sh
+++ cassearch.sh
@@ -6,6 +6,7 @@ for arg in "$@" ; do
     "--help"      ) set -- "$@" "-h"   ;;
     "--subroutine") set -- "$@" "-s"   ;;
     "--function"  ) set -- "$@" "-f"   ;;
+    "--interface" ) set -- "$@" "-i"   ;;
     "--no-locate" ) set -- "$@" "-L"   ;;
     *             ) set -- "$@" "$arg" ;;
   esac
@@ -14,6 +15,7 @@ done
 print_help=false ;
 subroutine_search=true ;
 function_search=true ;
+interface_search=true ;
 no_locate=false ;
 while getopts ":hgsfL" opt ; do
   case ${opt} in
@@ -23,14 +25,21 @@ while getopts ":hgsfL" opt ; do
     g ) # Just grep.
       subroutine_search=false ;
       function_search=false ;
+      interface_search=false ;
     ;;
     s ) # Subroutine only search.
       function_search=false ;
+      interface_search=false ;
     ;;
     f ) # Function only search.
       subroutine_search=false ;
+      interface_search=false
     ;;
-    L ) # Do not locate subroutine.
+    i ) # Interface only search.
+      subroutine_search=false ;
+      function_search=false ;
+    ;;
+    L ) # Do not locate.
       no_locate=true ;
     ;;
     \? )
@@ -52,11 +61,12 @@ if $print_help ; then
     echo 'Search for keywords in the files in the current directory.' ;
     echo 'Options optimised best for use in the CASTEP source code.' ;
     echo ;
-    echo '-f, --function       searches and locates for functions only'
+    echo '-f, --function       searches and locates functions only'
+    echo '-s, --subroutine     searches and locates subroutines only' ;
+    echo '-i, --interface      searches and locates interfaces only'
     echo '-g, --grep           just greps the files' ;
     echo '-h, --help           prints this help and exits' ;
-    echo '-L, --no-locate      will not vim into subroutine if found, redundant without --subroutine' ;
-    echo '-s, --subroutine     searches and locates for subroutines only' ;
+    echo '-L, --no-locate      will not vim into subroutine if found' ;
     exit 0 ;
 fi
 
@@ -108,7 +118,30 @@ fi
 
 
 
-if ( ! $subroutine_search && ! $function_search ) || [[ $((num_lines_sub+num_lines_func)) == 0 ]] ; then
+if $interface_search ; then
+    if $no_locate ; then
+        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}" "$i" ; done ;
+    else
+        temp_file=`mktemp` ;
+
+        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "interface\s+${1}" "$i" ; done >> $temp_file ;
+
+        num_lines_inter=`cat $temp_file | wc -l` ;
+        if [[ $num_lines_inter == 1 ]] ; then
+            file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
+            line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
+            vim +$line_num $file_to_open ;
+
+            exit 0 ;
+        else
+            for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}" "$i" ; done ;
+        fi
+    fi
+fi
+
+
+
+if ( ! $subroutine_search && ! $function_search && ! $interface_search ) || [[ $((num_lines_sub+num_lines_func+num_lines_inter)) == 0 ]] ; then
     for i in * ; do [ -f "$i" ] && \grep --line-number --with-filename --color=auto "${1}" "${i}" ; done
 fi
 
