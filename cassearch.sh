739e1403af7b220b398837b77549c68acc259b3b
Adam Dean <adam.dean@chem.ox.ac.uk>
2021-07-05 13:53:43 +0100

    Cassearch now works from anywhere

--- cassearch.sh
+++ cassearch.sh
@@ -58,8 +58,8 @@ done
 if $print_help ; then
     echo 'Usage: cassearch OPTION... [KEYWORD]...' ;
     echo ;
-    echo 'Search for keywords in the files in the current directory.' ;
-    echo 'Options optimised best for use in the CASTEP source code.' ;
+    echo 'Search for functions, subroutines, interfaces or files.' ;
+    echo 'Options best for use in the CASTEP source code.' ;
     echo ;
     echo '-f, --function       searches and locates functions only'
     echo '-s, --subroutine     searches and locates subroutines only' ;
@@ -67,29 +67,44 @@ if $print_help ; then
     echo '-g, --grep           just greps the files' ;
     echo '-h, --help           prints this help and exits' ;
     echo '-L, --no-locate      will not vim into subroutine if found' ;
+
     exit 0 ;
 fi
 
 
 
+function vim_or_cat_file {
+    if [[ "$2" == 1 ]] ; then
+        file_to_open=`cat "$1" | head -1 | cut -f1 -d:` ;
+        line_num=`cat "$1" | head -1 | cut -f2 -d:` ;
+        vim +$line_num $file_to_open ;
+        exit 0 ;
+    else
+        cat "$1" ;
+    fi
+}
+
+
+
+func_dir="/home/dean/work/castep-adean/Source/Functional/"
+fund_dir="/home/dean/work/castep-adean/Source/Fundamental/"
+util_dir="/home/dean/work/castep-adean/Source/Utility/"
+castep_file="/home/dean/work/castep-adean/Source/castep.f90"
+
+files=( "$castep_file" `for file in $(ls $func_dir) ; do echo "$func_dir$file" ; done` `for file in $(ls $fund_dir) ; do echo "$fund_dir$file" ; done` `for file in $(ls $util_dir) ; do echo "$util_dir$file" ; done` ) ;
+
+
+
 if $subroutine_search ; then
     if $no_locate ; then
-        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done ;
+        for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done ;
     else
         temp_file=`mktemp` ;
 
-        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "subroutine\s+${1}\s*\(" "$i" ; done >> $temp_file ;
+        for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done >> $temp_file ;
 
         num_lines_sub=`cat $temp_file | wc -l` ;
-        if [[ $num_lines_sub == 1 ]] ; then
-            file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
-            line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
-            vim +$line_num $file_to_open ;
-
-            exit 0 ;
-        else
-            for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "subroutine\s+${1}\s*\(" "$i" ; done ;
-        fi
+        vim_or_cat_file "$temp_file" "$num_lines_sub"
     fi
 fi
 
@@ -97,22 +112,14 @@ fi
 
 if $function_search ; then
     if $no_locate ; then
-         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done ;
+         for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done ;
      else
          temp_file=`mktemp` ;
 
-         for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "function\s+${1}\s*\(" "$i" ; done >> $temp_file ;
+         for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done >> $temp_file ;
 
          num_lines_func=`cat $temp_file | wc -l` ;
-         if [[ $num_lines_func == 1 ]] ; then
-             file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
-             line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
-             vim +$line_num $file_to_open ;
-
-             exit 0 ;
-         else
-             for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "function\s+${1}\s*\(" "$i" ; done ;
-         fi
+         vim_or_cat_file "$temp_file" "$num_lines_func"
      fi
 fi
 
@@ -120,29 +127,21 @@ fi
 
 if $interface_search ; then
     if $no_locate ; then
-        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}\b" "$i" ; done ;
+        for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}\b" "$i" ; done ;
     else
         temp_file=`mktemp` ;
 
-        for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp "interface\s+${1}\b" "$i" ; done >> $temp_file ;
+        for i in ${files[@]} ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}\b" "$i" ; done >> $temp_file ;
 
         num_lines_inter=`cat $temp_file | wc -l` ;
-        if [[ $num_lines_inter == 1 ]] ; then
-            file_to_open=`cat $temp_file | head -1 | cut -f1 -d:` ;
-            line_num=`cat $temp_file | head -1 | cut -f2 -d:` ;
-            vim +$line_num $file_to_open ;
-
-            exit 0 ;
-        else
-            for i in * ; do [ -f "${i}" ] && \grep --line-number --with-filename --extended-regexp --color=auto "interface\s+${1}\b" "$i" ; done ;
-        fi
+        vim_or_cat_file "$temp_file" "$num_lines_inter"
     fi
 fi
 
 
 
 if ( ! $subroutine_search && ! $function_search && ! $interface_search ) || [[ $((num_lines_sub+num_lines_func+num_lines_inter)) == 0 ]] ; then
-    for i in * ; do [ -f "$i" ] && \grep --line-number --with-filename --color=auto "${1}" "${i}" ; done
+    for i in ${files[@]} ; do [ -f "$i" ] && \grep --line-number --with-filename --color=auto "${1}" "${i}" ; done
 fi
 
 
