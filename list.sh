96a77d9912853e00273bfec99e81232c44f3aeed
Ava Dean <ava.dean@chem.ox.ac.uk>
2021-09-16 12:30:18 +0100

    Reformatting code

--- list.sh
+++ list.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-check=$(\ls -l . | \grep -c ^d)
+check=$(\ls -l . | \grep --count "^d")
 
-if [ $check == 0 ]
-	then
-		echo "No directories to list."
-	else
-		echo "DIR     ITEMS     SIZE"
+[ $check == 0 ] && { echo No directories to list && exit 1 ; } ;
 
-		for i in */ ; do
-			cd $i ;
-			count=$(\ls | wc -l) ;
-            #size=$(du -hs)
-			echo "${i:0:3}     $count         $size" ;
-			cd ../
-		done
-fi
+echo "DIR     ITEMS     SIZE"
 
+for i in */ ; do
+  count=$(\ls "$i" | wc -l) ;
+  #size=$(du -hs)
+  echo "${i:0:3}     $count         $size" ;
+done
 
