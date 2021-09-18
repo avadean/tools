29347923642320b9b42dc1ca5cab987eb55f400f
Ava Dean <ava.dean@chem.ox.ac.uk>
2021-09-18 14:14:43 +0100

    List can now take arguments and list n directories at once. No arguments defaults to listing the current directory

--- list.sh
+++ list.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 
-check=$(\ls -l . | \grep --count "^d")
+num=13
 
-[ $check == 0 ] && { echo No directories to list && exit 1 ; } ;
+function printDirCount {
+    printf "%-${num}s     %s     %s\n" "${1:0:${num}}" "$2" ;
+}
 
-echo "DIR     ITEMS     SIZE"
+#dir="DIR"
+#items="ITEMS"
+#spaces=$(( ($num - ${#dir}) / 2 ))
+#HEADER=$(printf "%-${spaces}s%-${spaces}s" "$dir" "$items")
+HEADER="     DIR         ITEMS"
 
-for i in */ ; do
-  count=$(\ls "$i" | wc -l) ;
-  #size=$(du -hs)
-  echo "${i:0:3}     $count         $size" ;
-done
+
+if (( $# == 0 )) ; then
+    count=$(\ls -l . | \grep --count "^d") ;
+
+    (( $count == 0 )) && { echo "No directories to list." ; exit 1 ; } ;
+
+    echo "$HEADER" ;
+
+    for i in */ ; do
+        count=$(\ls "$i" | wc -l) ;
+        printDirCount "$i" "$count" ;
+    done
+else
+    origDir=$(pwd) ;
+
+    # Check if there are any non-empty directories first.
+    for dir in "$@" ; do
+        [ -d "$dir" ] || continue ;
+        count=$(\ls -l "$dir" | \grep --count "^d") ;
+
+        # If there are some non-empty directories then have a header.
+        (( $count != 0 )) && { printf "  " ; echo "$HEADER" ; break ; } ;
+    done
+
+    # Now the main loop to output the results.
+    for dir in "$@" ; do
+        [ -d "$dir" ] || { echo "Skipping $dir as not a directory." ; continue ; } ;
+
+        echo "${dir}:" ;
+
+        (( $count == 0 )) && { echo "  No directories to list." ; continue ; } ;
+
+        cd "$dir" ;
+
+        for i in */ ; do
+            count=$(\ls "$i" | wc -l) ;
+            printf "  " ;
+            printDirCount "$i" "$count" ;
+        done
+
+        cd "$origDir" ;
+    done
+fi
 
