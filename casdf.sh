37beeaa33d68a3e2a2c018b345cf552617172629
Ava Dean <ava.dean@chem.ox.ac.uk>
2022-02-14 18:14:51 +0000

    Updated the castep git diff function to look cleaner and also fixed bug which meant it would only find .f90 files in the Fundamental directory

--- casdf.sh
+++ casdf.sh
@@ -5,26 +5,17 @@ fundamental="${castep_adean}Source/Fundamental/"
 functional="${castep_adean}Source/Functional/"
 utility="${castep_adean}Source/Utility/"
 
-function test_file {
-    #[ `\find ${1} -name ${2} | \wc --lines` -gt "0" ] ;
-
-    options=( "${fundamental}${1}.f90"  "${functional}${1}.f90"  "${utility}${1}.f90"
-              "${fundamental}${1}.F90"  "${functional}${1}.F90"  "${utility}${1}.F90" ) ;
-
-    for option in ${options} ; do
-        [ -f "${option}" ] && { \echo "${option}" ; exit 0 ; } ;
-    done
-
-    echo "ERROR" ;
-} ;
-
-cd $castep_adean ;
+cd "$castep_adean" ;
 
 [ -z "${1}" ] && { \git diff --word-diff=color ; exit 0 ; } ;
 
-file=$(test_file "${1}") ;
+options=( "${fundamental}${1}.f90"  "${functional}${1}.f90"  "${utility}${1}.f90"
+          "${fundamental}${1}.F90"  "${functional}${1}.F90"  "${utility}${1}.F90" ) ;
 
-[ "${file}" == "ERROR" ] && { \echo "Cannot find file relating to ${1}" ; exit 2 ; } ;
+for file in "${options[@]}" ; do
+    [ -f "${file}" ] && { \git diff --word-diff=color "${file}" ; exit 0 ; } ;
+done
 
-\git diff --word-diff=color "${file}"
+echo "Cannot find file relating to ${1}" ;
+exit 1 ;
 
