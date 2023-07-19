415f82a5915cca11879f05f2573d63a7ad75541d
Ava Dean <ava.dean@chem.ox.ac.uk>
2023-07-19 14:47:58 +0100

    Added more to reset castep.

--- resetCastep.sh
+++ resetCastep.sh
@@ -17,11 +17,18 @@ rm *.dfpt_* ;
 rm *.check_bak ;
 rm *.chdiff ;
 rm *.chdiff_fmt ;
+rm *.econv ;
 rm *.geom ;
 rm *.usp ;
 rm *.uspso ;
+rm *.beta ;
+rm *.gamma ;
+rm *.pwave ;
 rm fort.* ;
 rm *.sub ;
 rm *.md ;
 rm *.profile ;
+rm *.alpha_wvfn ;
+rm *.alpha_den ;
+rm *.pdos_weights ;
 
